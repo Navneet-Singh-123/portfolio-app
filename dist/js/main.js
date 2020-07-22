fad3e2292ca4a9a7753eb9d0d769997e0e8632ae
ns_01 <snavneet561@gmail.com>
2020-07-22 10:50:50 +0530

    Added google map on contact page

--- dist/js/main.js
+++ dist/js/main.js
@@ -28,3 +28,28 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+function initMap() {
+  var pune = { lat: 18.5204, lng: 73.8567 };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 6,
+    center: pune,
+  });
+
+  var contentString =
+    '<div id="content" style="color: black">' +
+    "<p>402, Nightingale, Nyati Enclave, Near DPS, Mohammedwadi</p>";
+  ("</div>");
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+
+  var marker = new google.maps.Marker({
+    position: pune,
+    map: map,
+  });
+  marker.addListener("click", function () {
+    infowindow.open(map, marker);
+  });
+}
